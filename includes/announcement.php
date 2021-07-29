d1cf604ad53a532370a6827bc7d50159d829a74a
Tomcoder123 <jaideepkollys@gmail.com>
2021-07-29 13:26:04 +0530

    d

--- includes/announcement.php
+++
@@ -1,5 +0,0 @@
-<div id="announcement-bar">
-	<div class="pageWidth animated fadeIn">
-		<i class="fa fa-bullhorn" aria-hidden="true"></i> Hey, this is a test announcement, you can change this in /includes/announcement.php
-	</div>
-</div>
